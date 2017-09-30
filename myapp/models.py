0de8fbe7f1c6fe7005dd7d59b656ec83195b1d31
吉村元聖 <strg.frontier@gmail.com>
2017-10-01 08:18:53 +0900

    add message_function

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Message_bord(models.Model):
+  new_message=models.TextField(null=False)
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .models import Message_bord
 
 def myapp_index(request):
-    return HttpResponse('Hello world')
+  msg = request.GET.get('words')
+  if msg is None:
+    data_list = Message_bord.objects.all()
+    
+    contexts = {
+      'result_list': data_list,
+    }
 
+    return render(request,'myapp/index.html',contexts)
+  else: 
+    message_data = Message_bord()
+    message_data.new_message = msg
+
+    message_data.save()
+
+    data_list = Message_bord.objects.all()
+    contexts = {
+      'result_list': data_list,
+    }
+    return render(request,'myapp/index.html',contexts)
 # Create your views here.
