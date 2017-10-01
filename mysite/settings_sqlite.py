cec1a17c36e1a4893eceeb831827b671858693fc
吉村元聖 <strg.frontier@gmail.com>
2017-10-01 09:27:25 +0900

    DBchange to MySQL

--- mysite/settings_sqlite.py
+++ mysite/settings_sqlite.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangodb',
-        'USER': 'django',
-        'PASSWORD': 'djangopass',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
