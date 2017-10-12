4f3f2b0a54aea6134a5198ab5eae77ab6c33d3b0
吉村元聖 <strg.frontier@gmail.com>
2017-10-12 16:34:45 +0900

    delete function

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.myapp_index,name='myapp_index'),
+    url(r'^delete$',views.myapp_delete,name='myapp_delete'),
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -6,7 +6,17 @@ from django.contrib.auth.validators import ASCIIUsernameValidator
 from .models import Message_bord
 from mysite.parser import *
 
+#削除機能
+@login_required
+def myapp_delete(request):
+  delete_text = request.POST.getlist('delete_text')
+  print(delete_text)
+  if delete_text:
+    Message_bord.objects.filter(id__in=delete_text).delete()   
 
+  return(myapp_index(request))
+
+#トップ画面
 @login_required
 def myapp_index(request):
   user_id = request.user.id
@@ -14,6 +24,7 @@ def myapp_index(request):
   user_name = user.get_username()
   print(user_name)
   msg = request.GET.get('words')
+  
 
   #文字列が入力されていなければ、現在のDB情報の取得のみ
   if msg is None:
