61cb3e1e4dc06e23865f0ee64fbdf86ac06736ab
吉村元聖 <strg.frontier@gmail.com>
2017-10-29 14:28:27 +0900

    Revert "adopt sqlite3"
    This reverts commit 96a9584fcb1f7fd9961af87ee1b808c5406a1cb5.

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'USER': 'django',
+        'PASSWORD': 'djangopass',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
