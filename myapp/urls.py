325045f203bf22fe0360a9849acdedd5a5c00867
吉村元聖 <strg.frontier@gmail.com>
2017-10-29 15:57:18 +0900

    call methods

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.myapp_index,name='myapp_index'),
-    url(r'^delete$',views.myapp_delete,name='myapp_delete'),
+    url(r'^$',views.index,name='index'),
 ]
 
